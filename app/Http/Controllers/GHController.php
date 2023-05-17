5fa656425570bff01df3f3ccfb94322b6b04bc5f
Rubén Jorge Verdeguer <st2021378@365.stucom.com>
2023-05-17 18:16:30 +0200

    github api finished

--- app/Http/Controllers/GHController.php
+++ app/Http/Controllers/GHController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class GHController extends Controller
 {
-    //
+    private string $accessToken;
+    private string $githubUser;
+    public function __construct()
+    {
+        $this->accessToken = env('TOKEN_GITHUB');
+        $this->githubUser = env('USER_GITHUB');
+    }
+    public function get_repositories(){
+        $client = new Client(['base_uri' => 'https://api.github.com/']);
+        $response = $client->request('GET', 'user/repos', [
+            "headers" => [
+                'Accept' => 'application/vnd.github.v3+json',
+                "Authorization" => "Bearer ".$this->accessToken
+            ]
+        ]);
+        return json_encode($response->getBody()->getContents());
+    }
+    public function get_collaborators_by_repo($repo){
+        $client = new Client(['base_uri' => 'https://api.github.com/']);
+        $response = $client->request('GET', "repos/". $this->githubUser ."/$repo/collaborators", [
+            "headers" => [
+                'Accept' => 'application/vnd.github.v3+json',
+                "Authorization" => "Bearer ".$this->accessToken
+            ]
+        ]);
+        return json_encode($response->getBody()->getContents());
+    }
+    public function create_new_repo($repoName){
+        $client = new Client(['base_uri' => 'https://api.github.com/']);
+        $response = $client->request('POST', "user/repos", [
+            "headers" => [
+                'Accept' => 'application/vnd.github.v3+json',
+                "Authorization" => "Bearer ".$this->accessToken
+            ],
+            "json" => [
+                'name' => "$repoName",
+                'description' => "repositorio de prueba para el ejercicio de M07"
+            ]
+            ]);
+        return json_encode($response->getBody()->getContents());
+    }
+    public function make_repository_private($repo){
+        $client = new Client(['base_uri' => 'https://api.github.com/']);
+        $response = $client->request('PATCH', "repos/".$this->githubUser."/$repo", [
+            "headers" => [
+                'Accept' => 'application/vnd.github.v3+json',
+                "Authorization" => "Bearer ".$this->accessToken
+            ],
+            "json" => [
+                'visibility' => 'private'
+            ]
+            ]);
+        return json_encode($response->getBody()->getContents());
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GHController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::prefix('/github')->group(
+    function () {
+        Route::get('/getRepositories', [GHController::class, 'get_repositories']);
+        Route::get('/getCollaborators/{repo}', [GHController::class, 'get_collaborators_by_repo']);
+        Route::post('/createRepo/{repoName}', [GHController::class, 'create_new_repo']);
+        Route::patch('/makePrivate/{repo}', [GHController::class, 'make_repository_private']);
+    }
+);
