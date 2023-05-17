2941519715634d9a7c438032a42e79e6a28f8381
Rubén Jorge Verdeguer <st2021378@365.stucom.com>
2023-05-17 19:17:31 +0200

    formula controller createdñ

---
+++ app/Http/Controllers/FormulaController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormulaController extends Controller
+{
+    public function get_all_drivers(){
+
+    }
+    public function get_all_teams(){
+
+    }
+    public function get_results_by_season_and_race(){
+
+    }
+    public function get_laptime_by_season_race_and_driver(){
+
+    }
+    public function get_standings_by_season(){
+
+    }
+    public function get_circuit_by_id(){
+        
+    }
+}
